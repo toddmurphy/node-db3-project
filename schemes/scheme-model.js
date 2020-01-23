f6fc44256ea47e6fa4c543e486c444af204bcc48
toddmurphy <toddmurphy93@gmail.com>
2020-01-22 20:58:58 -0400

    findby helper working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,25 @@
 const db = require('../data/db-config');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes').select();
+  //   return db.select('*').from('schemes');
+}
+
+function findById(id) {}
+
+function findSteps(id) {}
+
+function add(scheme) {}
+
+function update(changes, id) {}
+
+function remove(id) {}
