65e0e8437dc5a149a85628e4d4018179594f3ec4
toddmurphy <toddmurphy93@gmail.com>
2020-01-22 22:47:36 -0400

    update, gets get id , delete working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,12 +14,29 @@ function find() {
   //   return db.select('*').from('schemes');
 }
 
-function findById(id) {}
+function findById(id) {
+  return db('schemes').where('id', id);
+}
 
 function findSteps(id) {}
 
-function add(scheme) {}
+function add(scheme) {
+  return db('schemes').insert(scheme);
+}
 
-function update(changes, id) {}
+function update(changes, id) {
+  return db('schemes')
+    .where('id', id)
+    .update(changes)
+    .then(() => {
+      return findById(id)
+        .where('id', id)
+        .first();
+    });
+}
 
-function remove(id) {}
+function remove(id) {
+  return db('schemes')
+    .where('id', id)
+    .del();
+}
