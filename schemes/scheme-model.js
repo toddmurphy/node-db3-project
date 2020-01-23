07d70c95f8e8111fc4b7ac5e1790df4773b6b925
toddmurphy <toddmurphy93@gmail.com>
2020-01-22 23:52:56 -0400

    done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,11 +6,12 @@ module.exports = {
   findSteps,
   add,
   update,
-  remove
+  remove,
+  addStep
 };
 
 function find() {
-  return db('schemes').select();
+  return db('schemes');
   //   return db.select('*').from('schemes');
 }
 
@@ -18,10 +19,23 @@ function findById(id) {
   return db('schemes').where('id', id);
 }
 
-function findSteps(id) {}
+function findSteps(id) {
+  return (
+    db('steps')
+      //JOIN MUST GO FIRST --> 'steps' table is joined/connected to 'schemes' table
+      .join('schemes', 'steps.scheme_id', '=', 'schemes.id') //these linked by 'keys'
+      .select('scheme_name', 'step_number', 'instructions') //SELECT is allowing me to chose from each 'steps' & 'schemes' and pick whatver column and display in the order i write it
+      .where('scheme_id', id)
+      .orderBy('step_number') //order default ascending --> desc shows descending
+  );
+}
 
 function add(scheme) {
   return db('schemes').insert(scheme);
+}
+
+function addStep(step) {
+  return db('steps').insert(step);
 }
 
 function update(changes, id) {
